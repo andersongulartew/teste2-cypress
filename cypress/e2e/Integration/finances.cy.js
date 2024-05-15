f32e68ff741f34a48268f5d5081b31cb55f82eff
andersongulartew <andersongularte1993@gmail.com>
2024-05-15 20:24:35 -0300

    inclusão de gastos

--- cypress/e2e/Integration/finances.cy.js
+++ cypress/e2e/Integration/finances.cy.js
@@ -8,24 +8,44 @@ describe('Dev Finances Agilizei', () => {
 
     // Cadastrar entrada
         it('Cadastrar Entradas ', () => {
+
+            // Item  1
             cy.get('#transaction .button') // id + class
             .click()
-            .should('to.be.visible')
-
             cy.get('#description') // id
-            .type('Mesada')
-            .should('to.be.visible')
-
+            .type('Presente')
             cy.get('[name=amount]') // atributos
-            .type('20')
-            .should('to.be.visible')
-
+            .type('12')
             cy.get('[type=date]') // atributos
-            .type('2021-03-17')
-            .should('to.be.visible')
-
+            .type('2021-03-18')
             cy.contains('button', 'Salvar') // tipo e valor
             .click()
+
+            // Item 2
+            cy.get('#transaction .button') // id + class
+            .click()
+            cy.get('#description') // id
+            .type('Mesada')
+            cy.get('[name=amount]') // atributos
+            .type('50')
+            cy.get('[type=date]') // atributos
+            .type('2021-03-17')
+            cy.contains('button', 'Salvar') // tipo e valor
+            .click()
+
+            // Item 3
+            cy.get('#transaction .button') // id + class
+            .click()
+            cy.get('#description') // id
+            .type('Cortar Grama')
+            cy.get('[name=amount]') // atributos
+            .type('50')
+            cy.get('[type=date]') // atributos
+            .type('2021-03-19')
+            cy.contains('button', 'Salvar') // tipo e valor
+            .click()
+
+            cy.get('#data-table tbody tr').should('have.length', 3)
 
     });
 
